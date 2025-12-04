a6679c7972a928a43f2520637a702fa2c9e5427d
Israel Adewuyi <isistickz@gmail.com>
2025-12-04 22:04:20 +0000

    implemented mixed precision training

--- src/inno_swe_reasoner/coconut/train.py
+++ src/inno_swe_reasoner/coconut/train.py
@@ -1,6 +1,7 @@
 import time
 
 import torch
+from torch.cuda.amp import autocast
 from torch.nn.functional import cross_entropy
 
 from inno_swe_reasoner.coconut.coconut_utils import calculate_eot_offset
@@ -72,125 +73,139 @@ def train(config: CoconutTrainerConfig):
                 for idx, (prompt, cot_steps, answer) in enumerate(
                     zip(batch["prompt"], batch["cot_steps"], batch["answer"])
                 ):
-                    # Determine how many steps to replace with continuous thoughts
-                    # logger.info(f"Prompt: {prompt}\n\n")
-                    # logger.info(f"Cot steps: {cot_steps}\n\n")
-                    # logger.info(f"Answer: {answer}\n\n")
-                    steps_to_replace = min(stage, len(cot_steps))
-                    num_continuous_thoughts = steps_to_replace * config.data.c
-                    remaining_cot = cot_steps[steps_to_replace:]
+                    with autocast(dtype=torch.bfloat16):
+                        # Determine how many steps to replace with continuous thoughts
+                        steps_to_replace = min(stage, len(cot_steps))
+                        num_continuous_thoughts = steps_to_replace * config.data.c
+                        remaining_cot = cot_steps[steps_to_replace:]
 
-                    if stage == 0:
-                        # Stage 0: Regular CoT training (no continuous thoughts)
-                        input_ids, loss_mask = tokenize_data(
-                            tokenizer, prompt, cot_steps, answer
-                        )
-                        target_ids = (
-                            torch.tensor(input_ids.copy()[1:]).unsqueeze(0).to("cuda")
-                        )
-                        input_ids = torch.tensor(input_ids[:-1]).unsqueeze(0).to("cuda")
-                        loss_mask = torch.tensor(loss_mask).unsqueeze(0).to("cuda")
-                        position_ids = (
-                            torch.tensor(list(range(len(input_ids))))
-                            .unsqueeze(0)
-                            .to("cuda")
-                        )
-                        logger.info(f"Input IDs shape: {input_ids.shape}")
-                        assert input_ids.shape == target_ids.shape == loss_mask.shape, (
-                            f"input_ids, loss_mask and target_ids must have the same length, but got {input_ids.shape=}, {loss_mask.shape=}, {target_ids.shape=}"
-                        )
-
-                        # run forward pass on the tokens
-                        logits = model.embed_and_forward(input_ids, position_ids, False)
-                        B, L, V = logits.shape
-                        loss = cross_entropy(
-                            logits.view(-1, V), target_ids.view(-1), reduction="none"
-                        ).view(B, L)
-
-                        del logits
-                        del input_ids
-                        del position_ids
-                        del target_ids
-
-                        loss = loss[loss_mask].mean()
-                        batch_loss += loss
-                        logger.debug(f"Example {idx} LOSS = {loss.item()}")
-                    else:
-                        input_ids, _ = tokenize_data(
-                            tokenizer=tokenizer, prompt=prompt + "<bot>"
-                        )
-                        input_ids = torch.tensor(input_ids).unsqueeze(0).to("cuda")
-                        input_embed = model.get_embeddings(input_ids)
-
-                        # Generate continuous thoughts
-                        num_continuous_thoughts = 2  # TODO: Remove this later.
-                        logger.info(f"Shape of input_embed: {input_embed.shape}")
-                        for i in range(num_continuous_thoughts):
-                            hidden_states = model.forward_from_embeddings(
-                                input_embed, None, True
+                        if stage == 0:
+                            # Stage 0: Regular CoT training (no continuous thoughts)
+                            input_ids, loss_mask = tokenize_data(
+                                tokenizer, prompt, cot_steps, answer
                             )
-                            last_hidden = hidden_states[:, -1, :].unsqueeze(-2)
-                            input_embed = torch.cat([input_embed, last_hidden], dim=-2)
-
-                        eot_cot_toks, loss_mask = tokenize_data(
-                            tokenizer=tokenizer,
-                            cot_steps=["<eot>"] + remaining_cot,
-                            answer=answer,
-                        )
-                        eot_cot_toks = (
-                            torch.tensor(eot_cot_toks[:-1]).unsqueeze(0).to("cuda")
-                        )
-                        target_eot_cot = eot_cot_toks[:, 1:]
-                        eot_cot_toks = eot_cot_toks[:, :-1]
-
-                        assert target_eot_cot.shape == eot_cot_toks.shape, (
-                            "Tensor for input and target tokens for post latent thoughts should have the same shape"
-                        )
-
-                        eot_cot_embed = model.get_embeddings(eot_cot_toks)
-
-                        post_latent_embed = torch.cat(
-                            [input_embed, eot_cot_embed], dim=-2
-                        )
-
-                        logits = model.forward_from_embeddings(
-                            post_latent_embed, None, False
-                        )
-                        B, L, V = logits.shape
-
-                        num_prefix_tokens = input_embed.shape[-2]
-                        dummy_targets = torch.zeros(
-                            (B, num_prefix_tokens), dtype=torch.long, device="cuda"
-                        )
-                        target_ids = torch.cat([dummy_targets, target_eot_cot], dim=-1)
-                        num_prefix_tokens += eot_offset
-
-                        # Build loss mask: False for everything before <eot>, True for <eot> onward
-                        loss_mask = (
-                            torch.cat(
-                                [
-                                    torch.zeros(
-                                        num_prefix_tokens - 1, dtype=torch.bool
-                                    ),
-                                    torch.tensor(
-                                        loss_mask[eot_offset:], dtype=torch.bool
-                                    ),
-                                ]
+                            target_ids = (
+                                torch.tensor(input_ids.copy()[1:])
+                                .unsqueeze(0)
+                                .to("cuda")
                             )
-                            .unsqueeze(0)
-                            .to("cuda")
-                        )
+                            input_ids = (
+                                torch.tensor(input_ids[:-1]).unsqueeze(0).to("cuda")
+                            )
+                            loss_mask = torch.tensor(loss_mask).unsqueeze(0).to("cuda")
+                            position_ids = (
+                                torch.tensor(list(range(len(input_ids))))
+                                .unsqueeze(0)
+                                .to("cuda")
+                            )
+                            logger.info(f"Input IDs shape: {input_ids.shape}")
+                            assert (
+                                input_ids.shape == target_ids.shape == loss_mask.shape
+                            ), (
+                                f"input_ids, loss_mask and target_ids must have the same length, but got {input_ids.shape=}, {loss_mask.shape=}, {target_ids.shape=}"
+                            )
 
-                        loss = cross_entropy(
-                            logits.view(-1, V), target_ids.view(-1), reduction="none"
-                        ).view(B, L)
+                            # run forward pass on the tokens
+                            logits = model.embed_and_forward(
+                                input_ids, position_ids, False
+                            )
+                            B, L, V = logits.shape
+                            loss = cross_entropy(
+                                logits.view(-1, V),
+                                target_ids.view(-1),
+                                reduction="none",
+                            ).view(B, L)
 
-                        del logits
-                        del input_ids
-                        del target_ids
+                            del logits
+                            del input_ids
+                            del position_ids
+                            del target_ids
 
-                        loss = loss[loss_mask].mean()
-                        batch_loss += loss
+                            loss = loss[loss_mask].mean()
+                            batch_loss += loss
+                            logger.debug(f"Example {idx} LOSS = {loss.item()}")
+                        else:
+                            input_ids, _ = tokenize_data(
+                                tokenizer=tokenizer, prompt=prompt + "<bot>"
+                            )
+                            input_ids = torch.tensor(input_ids).unsqueeze(0).to("cuda")
+                            input_embed = model.get_embeddings(input_ids)
+
+                            # Generate continuous thoughts
+                            num_continuous_thoughts = 2  # TODO: Remove this later.
+                            logger.info(f"Shape of input_embed: {input_embed.shape}")
+                            for i in range(num_continuous_thoughts):
+                                hidden_states = model.forward_from_embeddings(
+                                    input_embed, None, True
+                                )
+                                last_hidden = hidden_states[:, -1, :].unsqueeze(-2)
+                                input_embed = torch.cat(
+                                    [input_embed, last_hidden], dim=-2
+                                )
+
+                            eot_cot_toks, loss_mask = tokenize_data(
+                                tokenizer=tokenizer,
+                                cot_steps=["<eot>"] + remaining_cot,
+                                answer=answer,
+                            )
+                            eot_cot_toks = (
+                                torch.tensor(eot_cot_toks[:-1]).unsqueeze(0).to("cuda")
+                            )
+                            target_eot_cot = eot_cot_toks[:, 1:]
+                            eot_cot_toks = eot_cot_toks[:, :-1]
+
+                            assert target_eot_cot.shape == eot_cot_toks.shape, (
+                                "Tensor for input and target tokens for post latent thoughts should have the same shape"
+                            )
+
+                            eot_cot_embed = model.get_embeddings(eot_cot_toks)
+
+                            post_latent_embed = torch.cat(
+                                [input_embed, eot_cot_embed], dim=-2
+                            )
+
+                            logits = model.forward_from_embeddings(
+                                post_latent_embed, None, False
+                            )
+                            B, L, V = logits.shape
+
+                            num_prefix_tokens = input_embed.shape[-2]
+                            dummy_targets = torch.zeros(
+                                (B, num_prefix_tokens), dtype=torch.long, device="cuda"
+                            )
+                            target_ids = torch.cat(
+                                [dummy_targets, target_eot_cot], dim=-1
+                            )
+                            num_prefix_tokens += eot_offset
+
+                            # Build loss mask: False for everything before <eot>, True for <eot> onward
+                            loss_mask = (
+                                torch.cat(
+                                    [
+                                        torch.zeros(
+                                            num_prefix_tokens - 1, dtype=torch.bool
+                                        ),
+                                        torch.tensor(
+                                            loss_mask[eot_offset:], dtype=torch.bool
+                                        ),
+                                    ]
+                                )
+                                .unsqueeze(0)
+                                .to("cuda")
+                            )
+
+                            loss = cross_entropy(
+                                logits.view(-1, V),
+                                target_ids.view(-1),
+                                reduction="none",
+                            ).view(B, L)
+
+                            del logits
+                            del input_ids
+                            del target_ids
+
+                            loss = loss[loss_mask].mean()
+                            batch_loss += loss
                 # After processing all examples in batch:
                 batch_loss = batch_loss / len(batch["prompt"])
                 batch_loss.backward()
